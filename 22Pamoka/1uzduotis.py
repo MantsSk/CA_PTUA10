de638660ce996fbf2ba097d88334f1b95f7082b6
MSkara <mantas.skara@gmail.com>
2024-01-07 14:38:10 +0200

    Create 1uzduotis.py

---
+++ 22Pamoka/1uzduotis.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name, age):
+        self.__name = self.set_name(name)
+        self.__age = self.set_age(age)
+    def set_age(self, age):
+        if 0 < age < 120:  # Simple validation logic
+            self.__age = age
+        else:
+            print("Invalid age")
+
+    def get_age(self):
+        return self.__age
+
+    def set_name(self, name):
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+    def canVote(self):
+        return self.__age >= 18
+
+
+person = Person("John", 22)
+print(person.get_age())
