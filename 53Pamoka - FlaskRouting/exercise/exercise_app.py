3faad9cf391fce7bd74f9651e0cdd78beacd9889
MSkara <mantas.skara@gmail.com>
2024-04-02 10:07:32 +0300

    Update

---
+++ 53Pamoka - FlaskRouting/exercise/exercise_app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/') #exercise 1
+def home():
+    return 'Welcome to the Home Page!'
+
+@app.route('/products/<category>') # exercise 1 
+def products(category):
+    return f'You selected the {category} category.'
+
+user_profiles = {
+    'john': ['Reading', 'Gardening'],
+    'jane': ['Cooking', 'Painting'],
+    'alice': ['Hiking', 'Photography']
+}
+
+@app.route('/profile/<username>') # exercise 2
+def profile(username):
+    if username in user_profiles:
+        hobbies = ', '.join(user_profiles[username])
+        return f'Hello {username}, your hobbies are: {hobbies}.'
+    else:
+        return 'User not found.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
