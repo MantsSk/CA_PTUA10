e1948d07b080f8c358aa11a8dc334f931b25bc55
Mantas Skara <mantas.skara@gmail.com>
2024-04-03 19:56:49 +0300

    54Pamoka

--- 53Pamoka - FlaskRouting/exercise/exercise_app.py
+++ 53Pamoka - FlaskRouting/exercise/exercise_app.py
@@ -11,9 +11,8 @@ def products(category):
     return f'You selected the {category} category.'
 
 user_profiles = {
-    'john': ['Reading', 'Gardening'],
-    'jane': ['Cooking', 'Painting'],
-    'alice': ['Hiking', 'Photography']
+    'john': ["Reading"],
+    'mantas': ["Teaching", "Basketball"]
 }
 
 @app.route('/profile/<username>') # exercise 2
@@ -21,8 +20,8 @@ def profile(username):
     if username in user_profiles:
         hobbies = ', '.join(user_profiles[username])
         return f'Hello {username}, your hobbies are: {hobbies}.'
-    else:
-        return 'User not found.'
+    else: 
+        return "User not found!"
 
 if __name__ == '__main__':
     app.run(debug=True)
